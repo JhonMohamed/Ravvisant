be44bd525ec0509b99afa1bd7a9ef1b2e543f2a1
Raydberg Gabriel <raydbergg@gmail.com>
2025-06-10 23:34:10 -0500

    implement bottom navigation

--- app/src/main/java/com/proyect/ravvisant/MainActivity.kt
+++ app/src/main/java/com/proyect/ravvisant/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.proyect.ravvisant
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Configuración del NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Actualiza los destinos de nivel superior
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.searchFragment,
+                R.id.cartFragment,
+                R.id.favoriteFragment,
+                R.id.profileFragment
+            )
+        )
+
+        // Configura el título de la barra superior
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Oculta el título predeterminado y muestra el personalizado
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Actualiza el título personalizado según el fragmento
+        val toolbarTitle = toolbar.findViewById<TextView>(R.id.toolbarTitle)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            toolbarTitle.text = destination.label
         }
+
+        // Configuración de la navegación inferior
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/proyect/ravvisant/features/auth/presentation/LoginActivity.kt
+++ app/src/main/java/com/proyect/ravvisant/features/auth/presentation/LoginActivity.kt
@@ -2,19 +2,24 @@ package com.proyect.ravvisant.features.auth.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.proyect.ravvisant.MainActivity
 import com.proyect.ravvisant.R
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val tvRegister =findViewById<TextView>(R.id.tvRegistrate)
-
+        val tvRegister = findViewById<TextView>(R.id.tvRegistrate)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
         tvRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
