d5b2027d2f1ee990090f72dcb5239d9a0ae26ce8
Raydberg Gabriel <raydbergg@gmail.com>
2025-06-11 00:32:43 -0500

    add layout and theme

--- app/src/main/java/com/proyect/ravvisant/MainActivity.kt
+++ app/src/main/java/com/proyect/ravvisant/MainActivity.kt
@@ -16,12 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Configuración del NavHostFragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Actualiza los destinos de nivel superior
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.homeFragment,
@@ -32,21 +30,17 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        // Configura el título de la barra superior
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        // Oculta el título predeterminado y muestra el personalizado
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        // Actualiza el título personalizado según el fragmento
         val toolbarTitle = toolbar.findViewById<TextView>(R.id.toolbarTitle)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             toolbarTitle.text = destination.label
         }
 
-        // Configuración de la navegación inferior
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.setupWithNavController(navController)
     }
